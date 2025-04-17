2c2bf3b12942f17ca920e29e467bd30ca2010614
nanini0 <alvaro.robles15081999@gmail.com>
2025-04-16 20:44:27 -0400

    html

--- diarioweb/urls.py
+++ diarioweb/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('registro/',views.Registro, name='registro'),
-    path('login/',views.LoginView.as_view(), name='login'),
+    path('registro/', views.Registro, name='registro'),
+    path('login/', views.LoginView.as_view(), name='login'),
     path('logout/',views.LogoutView.as_view(), name='logout')
 ]
